c86462834f074f84ff62dfc066fe7f9476233cc0
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-01-02 13:23:51 +0100

    start

---
+++ MyServicesLibrary/CitiesService.cs
@@ -0,0 +1,24 @@
+﻿namespace MyServicesLibrary;
+
+public class CitiesService
+{
+    private List<string> _cities;
+
+    public CitiesService()
+    {
+        _cities = new List<string>()
+        {
+            "New York",
+            "Los Angeles",
+            "Chicago",
+            "Houston",
+            "Phoenix"
+        };
+    }
+    
+    public List<string> GetCities()
+    {
+        return _cities;
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+namespace _6_aspnetcore_services_dependency_injection
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
